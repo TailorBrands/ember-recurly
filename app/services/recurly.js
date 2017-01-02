11cbcfa19d88dc8126eb02347e404179ee6a7f55
Nadav Shatz <nadavshatz@gmail.com>
2017-01-02 14:37:20 +0200

    Version 0.0.2

--- app/services/recurly.js
+++ app/services/recurly.js
@@ -1,12 +1,24 @@
+import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Service.extend({
   pricing: null,
 
+  setupFields() {
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      if (!config.recurly.publicKey) {
+        reject('RecurlyService: Missing Recurly key, please set `ENV.recurly.publicKey` in config.environment.js');
+      } else {
+        recurly.configure(config.recurly.publicKey);
+        resolve();
+      }
+    });
+  },
+
   getToken(billingInfo) {
     return new Ember.RSVP.Promise(function(resolve, reject) {
       recurly.token(billingInfo, function(err, token) {
-        if(err) {
+        if (err) {
           reject(err);
         } else {
           resolve(token);
@@ -18,7 +30,7 @@ export default Ember.Service.extend({
   getPayPalToken(opts) {
     return new Ember.RSVP.Promise(function(resolve, reject) {
       recurly.paypal(opts, function(err, token) {
-        if(err) {
+        if (err) {
           reject(err);
         } else {
           resolve(token);
