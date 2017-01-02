07ff3f05da14f2454537da298f5a2af4feae05dc
Nadav Shatz <nadavshatz@gmail.com>
2017-01-02 13:30:09 +0200

    V0.0.1

---
+++ app/services/recurly.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  pricing: null,
+
+  getToken(billingInfo) {
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      recurly.token(billingInfo, function(err, token) {
+        if(err) {
+          reject(err);
+        } else {
+          resolve(token);
+        }
+      });
+    });
+  },
+
+  getPayPalToken(opts) {
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      recurly.paypal(opts, function(err, token) {
+        if(err) {
+          reject(err);
+        } else {
+          resolve(token);
+        }
+      });
+    });
+  },
+
+  getPricing() {
+    this.pricing = this.pricing || recurly.Pricing();
+    return this.pricing;
+  }
+});
--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-recurly'
+  name: 'ember-recurly',
+  contentFor(type) {
+    // we use body since Recurly must be available before
+    if (type === 'body') {
+      return '<script src="https://js.recurly.com/v4/recurly.js"></script>';
+    }
+  }
 };
