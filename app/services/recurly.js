023bbd7fa92ea54dc4ac5ed618f1eaa83200f29b
Nadav Shatz <nadavshatz@gmail.com>
2017-01-17 13:52:34 +0200

    Going back to ad hoc setup

--- app/services/recurly.js
+++ app/services/recurly.js
@@ -4,6 +4,19 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   pricing: null,
 
+  setupFields(options) {
+    options = options || {};
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      if (!config.recurly.publicKey) {
+        reject('RecurlyService: Missing Recurly key, please set `ENV.recurly.publicKey` in config.environment.js');
+      } else {
+        options.publicKey = config.recurly.publicKey;
+        recurly.configure(options);
+        resolve();
+      }
+    });
+  },
+
   getToken(billingInfo) {
     return new Ember.RSVP.Promise(function(resolve, reject) {
       recurly.token(billingInfo, function(err, token) {
